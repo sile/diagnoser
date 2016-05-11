d3d9fea1abb93cbc783a5c19af6a7403ac9e33a4
Takeru Ohta <phjgt308@gmail.com>
2016-05-12 00:39:54 +0900

    Make dump-ast sub command template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate clap;
+
+use clap::App;
+use clap::Arg;
+use clap::SubCommand;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("diagnoser")
+                      .subcommand(SubCommand::with_name("dump-ast")
+                                      .arg(Arg::with_name("BEAM_FILE")
+                                               .required(true)
+                                               .index(1)))
+                      .get_matches();
+    if let Some(matches) = matches.subcommand_matches("dump-ast") {
+        unimplemented!()
+    } else {
+        println!("{}", matches.usage());
+        std::process::exit(1);
+    }
 }
