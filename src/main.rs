a6f083d9a35f1a93ad3cbc1858a5ee225ae25185
Takeru Ohta <phjgt308@gmail.com>
2022-11-21 20:53:06 +0900

    Update clap version

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,32 @@
 extern crate clap;
 extern crate diagnoser;
 
-use clap::App;
-use clap::Arg;
-use clap::SubCommand;
+use clap::Parser;
 use diagnoser::env::Env;
 use diagnoser::module::Module;
 
+#[derive(Parser)]
+enum Args {
+    DumpAst { beam_file: String },
+    Analyze { beam_file: Vec<String> },
+}
+
 fn main() {
-    let matches = App::new("diagnoser")
-        .subcommand(SubCommand::with_name("dump-ast").arg(Arg::with_name("BEAM_FILE")
-            .required(true)
-            .index(1)))
-        .subcommand(SubCommand::with_name("analyze").arg(Arg::with_name("BEAM_FILE")
-            .required(true)
-            .takes_value(true)
-            .multiple(true)))
-        .get_matches();
-    if let Some(matches) = matches.subcommand_matches("dump-ast") {
-        let beam_file = matches.value_of("BEAM_FILE").unwrap();
-        let module = Module::from_beam_file(beam_file)
-            .expect(&format!("Can't parse file: {}", beam_file));
-        println!("{:?}", module);
-    } else if let Some(matches) = matches.subcommand_matches("analyze") {
-        let mut env = Env::new();
-        for beam_file in matches.values_of("BEAM_FILE").unwrap() {
-            println!("LOAD: {}", beam_file);
-            let module = Module::from_beam_file(beam_file)
+    let args = Args::parse();
+    match args {
+        Args::DumpAst { beam_file } => {
+            let module = Module::from_beam_file(&beam_file)
                 .expect(&format!("Can't parse file: {}", beam_file));
-            env.add_module(module);
+            println!("{:?}", module);
         }
-    } else {
-        println!("{}", matches.usage());
-        std::process::exit(1);
+        Args::Analyze { beam_file } => {
+            let mut env = Env::new();
+            for beam_file in &beam_file {
+                println!("LOAD: {}", beam_file);
+                let module = Module::from_beam_file(beam_file)
+                    .expect(&format!("Can't parse file: {}", beam_file));
+                env.add_module(module);
+            }
+        }
     }
 }
